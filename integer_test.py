c3f4b0a506a3ae3f816b9be3efab5a807bf2c7a0
noy <noy@mma.club.uec.ac.jp>
2015-12-19 17:14:52 +0900

    add integer_test.py

---
+++ integer_test.py
@@ -0,0 +1,18 @@
+import unittest
+import sample
+
+class SampleTest(unittest.TestCase):
+    def setUp(self):
+        self.util = sample.IntUtil()
+    def test_twice(self):
+        # 足した数と同値か
+        import random
+        roop = 0
+        while roop != 10000:
+            n = random.randint(0, 100)
+            ans = n + n
+            self.assertEqual(self.util.twice(n), ans)
+            roop += 1
+
+if __name__ == '__main__':
+    unittest.main()
